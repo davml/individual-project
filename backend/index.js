60bf8507392fd90b7f7409966d3c7fca42e942cb
gvb17187 <david.lewis.2017@uni.strath.ac.uk>
2021-03-24 19:36:17 +0000

    test

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,8 @@ app.use('/job', jobRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+console.log("Hello");
+
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     () => {
         app.listen(PORT, ()=>{console.log(`Server running on port: ${PORT}`)})
