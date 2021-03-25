b0c5b3ae4f14e0b63198f618109db95ccd5750fb
gvb17187 <david.lewis.2017@uni.strath.ac.uk>
2021-03-25 18:47:57 +0000

    fixed substr showing error

--- frontend/src/components/layout/body/pages/search/jobsList/jobItem/jobItem.js
+++ frontend/src/components/layout/body/pages/search/jobsList/jobItem/jobItem.js
@@ -11,13 +11,13 @@ const JobItem = (props) => {
     return (
         <div className={classes["job-item"]}>
             <div className={classes["job-title"]}>
-                <p>Title: {job?.title}.substring(0,75)</p>
+                <p>Title: {job?.title.substring(0,75)}</p>
             </div>
             <div className={classes["job-description"]}>
                 <p>Description: {job?.description.substring(0,75)}</p>
             </div>
             <div className={classes["job-payment"]}>
-                <p>Payment: {job?.payment}.substring(0,75)</p>
+                <p>Payment: {job?.payment.substring(0,75)}</p>
             </div>
             <div className={classes["job-more"]}>
                 <button onClick={toggleModal}>See More</button>
